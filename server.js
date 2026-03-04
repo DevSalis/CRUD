cfca00cadbf56ee5988468c7362bae23aef9b4fb
MrSalis <philip.cost@hotmail.com>
2026-03-04 17:20:15 -0300

    Rota post

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ app.listen(3000);
 app.get("/usuarios", (req, res) => {
   res.send("rota get criada com sucesso");
 });
+
+app.post("/usuarios", (req, res) => {
+  res.send("rota post criada com sucesso");
+});
