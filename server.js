f939934c1d2547bdaca76a224f00b38b25169111
MrSalis <philip.cost@hotmail.com>
2026-04-17 22:57:11 -0300

    banco de dados ok. rota C&R ok

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 
+const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 app.listen(3000);
-const user = [];
-const prisma = new PrismaClient();
 
-app.get("/usuarios", (req, res) => {
+app.get("/usuarios", async (req, res) => {
+  const user = await prisma.user.findMany();
   res.status(200).json(user);
 });
 
-app.post("/usuarios", (req, res) => {
-  user.push(req.body);
-  res.status(201).json({ message: "Usuario cadastrado com sucesso" });
+app.post("/usuarios", async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age,
+    },
+  });
+  res.status(201).json(user);
 });
